c6791bdea3344defcfd372d227ab9059c4d08e67
Jelte Boelens <129329@hondsrugcoolege.nl>
2016-06-20 16:27:17 +0200

    Added Switchable months only works in the current year

--- index.php
+++ index.php
@@ -25,39 +25,54 @@ include("inc_db_HomeWorkSite.php");
         <?php include("HomeWorkInput.php"); ?>
     </div>
     <div id="HomeWorkCalander">
-	<?php
-		$CurrentMonth = date("m");
-		$MonthName = date('F', mktime(0, 0, 0, $CurrentMonth, 10));
-		
-		
-		for($i=1;$i<=cal_days_in_month(CAL_GREGORIAN, $CurrentMonth, 2016); $i++){
-			echo "<div class='CalanderDay'><div class='Date'>";
-			if($i== date("d") && date("m")== $CurrentMonth){
-				echo "<span style='color: red; font-weight: bold;'>$i ". $MonthName."<br/></span>";
-			}else{
-				echo "$i ". $MonthName."<br/>";
+		<?php
+			if(!isset($_SESSION["CurrentMonth"])){
+				$_SESSION["CurrentMonth"] = date("m");
 			}
-			echo "</div>";
-			echo"<div class= 'HomeWork'>";
-			$query = "SELECT HomeWork, Datum, HomeWorkDone, ID, UserId FROM HomeWork";
-			$hoer = mysqli_query($DBConnect, $query);
-			while($row = mysqli_fetch_array($hoer))
-			{
-				$Date = $row[1];
-				$DateArray = explode("-",$Date);
-				if(isset($_SESSION["UserId"])){
-					if($i == $DateArray[2] && $row[4]== $_SESSION["UserId"]){
-						if($row[2]==0){
-							echo"<a href='index.php?id=".$row[3]."&To=Done'><span class='HomeWorkToDo'>".$row[0]."<br/></span></a>";
-						}else{
-							echo"<a href='index.php?id=".$row[3]."&To=NotDone'<span class='HomeWorkDone'>".$row[0]."<br/></span></a>";
-						}
+			if($_SERVER["REQUEST_METHOD"] == "GET"){
+				if(isset($_GET["Change"])){
+					if($_GET["Change"]=="back"){
+						$_SESSION["CurrentMonth"]--;
+					}else{
+						$_SESSION["CurrentMonth"]++;
 					}
 				}
 			}
-			echo"</div></div>";
-		}
-	?>
+			$CurrentMonth = $_SESSION["CurrentMonth"];
+			$MonthName = date('F', mktime(0, 0, 0, $CurrentMonth, 10));
+			
+			echo "<div class='CurrentMonth'>";
+			echo "<a href='index.php?Change=back'><-</a>".$MonthName."<a href='index.php?Change=forward'>-></a>";
+			echo "</div><hr/>";
+			
+			for($i=1;$i<=cal_days_in_month(CAL_GREGORIAN, $CurrentMonth, 2016); $i++){
+				echo "<div class='CalanderDay'><div class='Date'>";
+				if($i== date("d") && date("m")== $CurrentMonth){
+					echo "<span style='color: red; font-weight: bold;'>$i ". $MonthName."<br/></span>";
+				}else{
+					echo "$i ". $MonthName."<br/>";
+				}
+				echo "</div>";
+				echo"<div class= 'HomeWork'>";
+				$query = "SELECT HomeWork, Datum, HomeWorkDone, ID, UserId FROM HomeWork";
+				$hoer = mysqli_query($DBConnect, $query);
+				while($row = mysqli_fetch_array($hoer))
+				{
+					$Date = $row[1];
+					$DateArray = explode("-",$Date);
+					if(isset($_SESSION["UserId"])){
+						if($i == $DateArray[2] && $row[4]== $_SESSION["UserId"] && $DateArray[1]==$CurrentMonth){
+							if($row[2]==0){
+								echo"<a href='index.php?id=".$row[3]."&To=Done'><span class='HomeWorkToDo'>".$row[0]."<br/></span></a>";
+							}else{
+								echo"<a href='index.php?id=".$row[3]."&To=NotDone'<span class='HomeWorkDone'>".$row[0]."<br/></span></a>";
+							}
+						}
+					}
+				}
+				echo"</div></div>";
+			}
+		?>
     </div>
 </body>
 <?php
